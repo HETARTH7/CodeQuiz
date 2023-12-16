4fb815c57ec36a3bf1a1549dd064d8a3c572bed0
unknown <hetarth002@gmail.com>
2023-12-16 18:53:08 +0530

    add login

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+let user = localStorage.getItem("name");
+let form = document.getElementById("form");
+let game = document.getElementById("game");
 
+if (user) {
+  form.hidden = true;
+  game.hidden = false;
+} else {
+  game.hidden = true;
+  document.body.classList.add("login");
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  if (!name) alert("You must enter your name");
+  else {
+    localStorage.setItem("name", name);
+    form.hidden = true;
+    game.hidden = false;
+    document.body.classList.remove("login");
+  }
+});
